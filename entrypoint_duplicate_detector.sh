2a04bec034924fa8fe681b56d053e70303f28f78
rdar <36108848+rdar-lab@users.noreply.github.com>
2026-01-17 18:39:42 +0200

    Support all parameters in docker

--- entrypoint_duplicate_detector.sh
+++ entrypoint_duplicate_detector.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/bash
 set -e
 
-# Populate ARGS array from script arguments
-ARGS=("$@")
-
-# Now safely get the directory
-TARGET_DIR=${ARGS[0]:-/data}
-
 # Build command with appropriate flags using array
 # Use -u flag for unbuffered output so logs appear immediately in Docker
 CMD_ARGS=('python3' '-u' 'duplicate_detector.py')
 
-CMD_ARGS+=("$TARGET_DIR")
+# Add all the arguments that were passed to the script
+CMD_ARGS+=("$@")
 
 # Add thumbnails directory (default to /thumbs in Docker)
 CMD_ARGS+=('--thumbnails-dir' '/thumbs')
